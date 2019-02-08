42fbc2c55448cf09461c0d3897a10812fb659faa
Seolhee Kim <seolheekim808@gmail.com>
2019-02-07 17:06:00 -0800

    imported SongList file

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import SongList from './SongList';
+
 
 const App = () => {
-  return <div> APP </div>
-}
+  return (
+    <div>
+      <SongList />
+    </div>
+  )
+};
 
 export default App;
 
