e5e599145b87bbac38dfd8505511046eb538cb93
Seolhee Kim <seolheekim808@gmail.com>
2019-02-07 17:23:10 -0800

    created renderList() menthod to display song list

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,8 +2,28 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends Component {
+
+  // This method is to just take the list of songs,
+  // map over them and return a big blob of JSX.
+  renderList() {
+    return this.props.songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary" >
+              SELECT
+            </button>
+          </div>
+
+          <div className="content">{song.title}</div>
+        </div>
+      )
+    })
+  };
+
+
   render() {
-    return <div> songList </div>
+    return <div className="ui divided list"> {this.renderList()} </div>
   }
 };
 
